330afcc031abcd493dd08158e9339c9c13be5ad7
Juan Pablo Rodriguez <enano17711@gmail.com>
2023-06-16 23:50:20 -0400

    complete order controller

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -38,6 +38,7 @@ builder.Services.AddDbContext<StoreDbContext>(opts =>
         builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
 });
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+builder.Services.AddScoped<IOrderRepository, EFOrderRepository>();
 builder.Services.AddRazorPages();
 
 builder.Services.AddDistributedMemoryCache();
